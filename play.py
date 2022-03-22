7dc5a8311d75e7da483c4c2db8de1176d10d011c
walidbou6 <wali.bousseta@gmail.com>
2022-03-22 08:22:16 +0100

    feat: draw XO when no winner

--- play.py
+++ play.py
@@ -4,6 +4,7 @@ import numpy as np
 from constants import *
 from draws import *
 from logic import *
+import time
 
 
 pygame.init()
@@ -23,8 +24,6 @@ board = np.zeros((BOARD_ROWS, BOARD_COLS))
 draw_lines(screen)
 player = 1
 game_over = False
-
-# pygame.draw.line(screen, CROSS_COLOR, (WIDTH//4, WIDTH//4), (WIDTH//3, WIDTH//3), CROSS_WIDTH)
                     
 # mainloop
 while True:
@@ -44,15 +43,22 @@ while True:
                 mark_square(board, clicked_row, clicked_col, player)
                 if check_win(screen, board, player):
                     game_over = True
+                    # time.sleep(2)
+                    # screen.fill(BG_COLOR)
+                    # board = np.zeros((BOARD_ROWS, BOARD_COLS))
+                    # text = font.render('WINNER!', False, TEXT_COLOR)
+                    # screen.blit(text, (WIDTH//6, WIDTH//6))
+
+
                 player = player % 2 + 1
                 draw_figures(screen, board)
                 if is_board_full(board) and not check_win(screen, board, player):
                     screen.fill(BG_COLOR)
                     text = font.render('DRAW!', False, TEXT_COLOR)
                     screen.blit(text, (WIDTH//4, WIDTH//4))
-                    # pygame.draw.circle(screen, CIRCLE_COLOR, (WIDTH//4, WIDTH//2), CIRCLE_RADIUS//2, CIRCLE_WIDTH//2)
-                    # pygame.draw.line(screen, CROSS_COLOR, (WIDTH//4+SPACE, WIDTH//4+SPACE), (SQUARE_SIZE+SQUARE_SIZE-SPACE, SQUARE_SIZE+SPACE), CROSS_WIDTH)
-                    # pygame.draw.line(screen, CROSS_COLOR, (SQUARE_SIZE+SPACE, SQUARE_SIZE+SPACE), (SQUARE_SIZE+SQUARE_SIZE-SPACE, SQUARE_SIZE+SQUARE_SIZE-SPACE), CROSS_WIDTH)
+                    pygame.draw.circle(screen, CIRCLE_COLOR, (WIDTH//2+3*SPACE//2, WIDTH//2+SPACE//2), CIRCLE_RADIUS, CIRCLE_WIDTH)
+                    pygame.draw.line(screen, CROSS_COLOR, start_pos=(SQUARE_SIZE, SQUARE_SIZE+3*SPACE//2), end_pos=(2*SQUARE_SIZE-2*SPACE, 2*SQUARE_SIZE-SPACE//2), width=CROSS_WIDTH)
+                    pygame.draw.line(screen, CROSS_COLOR, start_pos=(SQUARE_SIZE, 2*SQUARE_SIZE-SPACE//2), end_pos=(2*SQUARE_SIZE-2*SPACE, SQUARE_SIZE+3*SPACE//2), width=CROSS_WIDTH)
 
 
 
